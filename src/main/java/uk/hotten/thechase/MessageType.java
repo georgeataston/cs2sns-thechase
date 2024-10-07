2ce40c7f1229e6664677894f432fc07574dc210e
George Hotten <github@ghott.me>
2024-10-07 16:41:16 +0100

    messagetype comments

--- src/main/java/uk/hotten/thechase/MessageType.java
+++ src/main/java/uk/hotten/thechase/MessageType.java
@@ -2,13 +2,15 @@ package uk.hotten.thechase;
 
 public enum MessageType {
 
-    TEXT(0),
-    QUESTION_SEND(1),
-    QUESTION_RECEIVE(2),
-    QUESTION_START(3),
-    PLAYER_ANSWER(4),
-    TIMER(5),
-    QUESTION_STOP(6);
+    TEXT(0),              // Displays text on the client
+    QUESTION_SEND(1),     // Sending the question from the server to the client
+    QUESTION_RECEIVE(2),  // Clients confirms receipt of the question
+    QUESTION_START(3),    // Sever informs the client to show the question
+    PLAYER_ANSWER(4),     // Client sends a player's answer
+    PLAYER_ANSWERED(5),   // Server informs client a player has answered
+    TIMER(6),             // Server sending timer countdown
+    QUESTION_STOP(7),     // Server sends to prevent question answering
+    RESULTS_SEND(8);      // Server sends the results for the clients to show
 
 
     public int id;
